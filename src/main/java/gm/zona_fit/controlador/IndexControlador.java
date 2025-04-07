da979fecd1780b8dac7f992b2cc56c1c20e058f3
Checho buitrago <buitrago12.09@gmail.com>
2025-04-07 10:38:19 -0500

    Boton, eliminar, actualizar. Commit Final. Proyecto terminado

--- src/main/java/gm/zona_fit/controlador/IndexControlador.java
+++ src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.slf4j.LoggerFactory;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 import gm.zona_fit.servicio.IClienteServicio;
 import gm.zona_fit.modelo.Cliente;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
 
@@ -37,5 +40,39 @@ public class IndexControlador {
 
     public void agregarCliente(){
         this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("Cliente a guardar: "+this.clienteSeleccionado);
+        //Agregar
+        if (this.clienteSeleccionado.getId() == null) {
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage("Cliente agregado"));
+        }
+        // Modificar (update)
+        else {
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente Actualizado") );
+        }
+        //Ocultar ventana modal
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+        // Actualizar tabla usando ajax
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes","forma-clientes:clientes-tabla");
+        // Reset objeto de Cliente seleccionado
+        this.clienteSeleccionado = null;
+    }
+
+    public void eliminarCliente(){
+        logger.info("Cliente a eliminar "+this.clienteSeleccionado);
+        this.clienteServicio.eliminarCliente(this.clienteSeleccionado);
+        // Eliminar el registro de la lista de clientes
+        this.clientes.remove(this.clienteSeleccionado);
+        // Reset del objeto de cliente seleccionado
+        this.clienteSeleccionado = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("cliente Eliminado"));
+        PrimeFaces.current().ajax().update("forma-cliente:mensajes",
+        "forma-clientes:clientes-tabla");
     }
 }
